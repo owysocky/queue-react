9decf44ea3701438c0f5cc5dc6ef34c78815d839
ow <volovykolya@gmail.com>
2019-04-27 16:22:44 -0700

    add styles

--- src/components/ConfirmationQuestions.jsx
+++ src/components/ConfirmationQuestions.jsx
@@ -4,8 +4,22 @@ import PropTypes from 'prop-types';
 function ConfirmationQuestions(props){
   return (
     <div>
-      <p>Have you gone through all the steps on the Learn How to Program debugging lesson?</p>
-      <button onClick={props.onTroubleshootingConfirmation}>Yes</button>
+    <style>{`
+      div{
+        background-color: #9fbff2;
+        padding: 10px;
+      }
+      h3{
+        color: #6d5cf2;
+      }
+      .btn{
+        background-color: #6d5cf2;
+        color: white;
+        padding: 20px;
+      }
+    `}</style>
+      <h3>Have you gone through all the steps on the Learn How to Program debugging lesson?</h3>
+      <button className="btn" onClick={props.onTroubleshootingConfirmation}>Yes</button>
     </div>
   );
 }
