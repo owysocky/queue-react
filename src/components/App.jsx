d7504d3edaaada8e175bdb89fa9b91da32e608a3
ow <volovykolya@gmail.com>
2019-05-01 21:47:24 -0700

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,7 +11,8 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      masterTicketList: []
+      masterTicketList: [],
+      selectedTicket: null
     };
     this.handleAddingNewTicketToList = this.handleAddingNewTicketToList.bind(
       this
